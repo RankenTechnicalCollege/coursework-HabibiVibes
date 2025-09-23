56b51fdbd4ef9ba13c974bc7ed0ab739a4134b5c
Dillon Anderson <dillon_anderson@insideranken.org>
2025-09-22 19:42:41 -0500

    CH 10 extend

--- html-css/data-files/chapter10/extend/scripts/script.js
+++ html-css/data-files/chapter10/extend/scripts/script.js
@@ -1,5 +1,15 @@
 /*
-    Student Name:
+    Student Name: Dillon Anderson
     File Name: script.js
-    Date: 
+    Date: 09/22/2025
 */
+
+/* jQuery hide and show method */
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("article p").hide();
+    });
+    $("#show").click(function(){
+        $("article p").show();
+    });
+});
