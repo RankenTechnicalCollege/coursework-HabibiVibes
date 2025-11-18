adf702305f1219259e2f484a4392632ba8460195
Dillon Anderson <dillon_anderson@insideranken.org>
2025-11-18 14:02:48 -0600

    JavaScript CH8

--- javascript/data-files/Chapter08/js08/project03/project08-03.js
+++ javascript/data-files/Chapter08/js08/project03/project08-03.js
@@ -4,16 +4,56 @@
       Project 08-03
 
       Project to build a pizza using object oriented programming
-      Author: 
-      Date:   
+      Author: Dillon Anderson 
+      Date:   11/18/2025
 
       Filename: project08-03.js
 */
 
 /*---------------- Object Code ----------------------*/
 
+// 3. create the cart object with an addItem() method
+let cart = {
+   items: [],
 
+   addItem(foodItem) {
+      this.items.push(foodItem);
+   }
+};
 
+// 4. constructor function for Pizza objects
+function Pizza() {
+   this.size = "";
+   this.crust = "";
+   this.toppings = [];
+}
+
+// 5. constructor function for Topping objects
+function Topping() {
+   this.name = "";
+   this.side = "";
+}
+
+// 6. addToCart() method to the Pizza prototype
+Pizza.prototype.addToCart = function() {
+   cart.items.push(this);
+};
+
+// 7. summarize() method added to the pizza prototype
+Pizza.prototype.summarize = function() {
+   let summary = "Pizza";
+
+   // Add size and crust to the summary
+   summary += this.size + " " + this.crust + " ";
+
+   // loop through the toppings and add name and side to the summary
+   for (let i = 0; i < this.toppings.length; i++) {
+      let name = this.toppings[i].name;
+      let side = this.toppings[i].side;
+      summary += name + " (" + side + ") ";
+   }
+   return summary;
+};
 
 
 
@@ -77,15 +117,44 @@ function drawPizza() {
 
 
 
-// Function to build the pizza
+// 8. Function to build the pizza
 function buildPizza() {
    let checkedToppings = document.querySelectorAll("input.topping:checked"); 
 
+   // Create a new Pizza object
+   let myPizza = new Pizza();
+   myPizza.size = pizzaSizeBox.value;
+   myPizza.crust = pizzaCrustBox.value;
 
+   // add selected toppings as Topping objects to the pizza
+   for (let i = 0; i < checkedToppings.length; i++) {
+      let myTopping = new Topping();
+      myTopping.name = checkedToppings[i].name;
+      myTopping.side = checkedToppings[i].value;
+      myPizza.toppings.push(myTopping);
+   }
+
+   return myPizza;
 }    
 
-// Function to add the built pizza to the shopping cart
+// 9. Function to add the built pizza to the shopping cart
 function updateCart() {
 
+   // a. Build the pizza
+   let myPizza = buildPizza();
 
+   // b. Add the pizza to the cart
+   cart.addItem(myPizza);
+
+   // c. log the cart to the console 
+   console.log(cart);
+
+   // d. display the pizza summary in the cartBox
+   let p = document.createElement("p");
+   p.textContent = myPizza.summarize();
+   cartBox.appendChild(p);
+
+   // e. reset the pizza builder interface
+   clearPizzaImage();
+   clearToppings();
 }  
